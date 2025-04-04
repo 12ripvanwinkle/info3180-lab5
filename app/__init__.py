9216c38f4cb56fa1eeea8ee815ac27f34c81cf1f
Nitesh <niteshanthony3@gmail.com>
2025-04-03 21:41:47 -0500

    Completed exercise 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+app.config['SECRET_KEY'] = 'Som3$ec5etK*y'  # Required for Flask-WTF
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lab5_user:password@localhost/lab5'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
 from app import views
+from app.models import Movie  # Import your model
--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
-# Add any model classes for Flask-SQLAlchemy here
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from app import db  # Make sure this matches how you're importing db in your app
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
